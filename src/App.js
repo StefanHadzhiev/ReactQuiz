788cd848f6a5ad741db6ce04b128740f0b049313
StefanHadzhiev <stefanhadzhiev24@gmail.com>
2024-09-02 08:24:35 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Main from "./Main";
+import Header from "./Header";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
+  );
+}
